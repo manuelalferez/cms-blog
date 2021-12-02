f13bf8cf56c6526128994c44b556f987ae53bb5a
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-12-02 18:20:21 +0100

    Fixing header and redesigning footer

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   data.links.forEach((item) =>
     list.push(
       <Link href={item.url}>
-        <a className="z-10 p-2 mr-10 text-mywhite hover:bg-mylightpink">
+        <a className="z-10 p-2 md:mr-10 text-mywhite hover:bg-mylightpink">
           {item.text}
         </a>
       </Link>
@@ -14,11 +14,11 @@ const Header = () => {
   );
 
   return (
-    <div className="flex justify-center w-screen relative text-base md:text-lg md:pt-4 lg:pt-8 xl:pt-10 font-display">
+    <div className="flex justify-center w-screen relative text-base md:text-lg md:pt-4 font-display">
       {list}
       <img
         src={data.bgURL}
-        class="absolute top-0 w-screen z-0 transform rotate-180	"
+        class="absolute top-0 w-screen z-0 h-16 md:h-20 transform rotate-180	"
       />
     </div>
   );
---
+++ data/footer.js
@@ -0,0 +1,44 @@
+export default {
+  social: [
+    {
+      alt: "Twitter",
+      imageURL:
+        "https://ik.imagekit.io/manuelalferez/blog/social/twitter_1nkyOK9ga.svg?updatedAt=1638464125509",
+      url: "https://twitter.com/manuelalferez",
+    },
+    {
+      alt: "GitHub",
+      imageURL:
+        "https://ik.imagekit.io/manuelalferez/blog/social/github_1_utdAxdrIS.svg?updatedAt=1638464125092",
+      url: "https://github.com/manuelalferez",
+    },
+    {
+      alt: "Telegram",
+      imageURL:
+        "https://ik.imagekit.io/manuelalferez/blog/social/telegram_1_qutiXC4ot.svg?updatedAt=1638464125400",
+      url: "https://t.me/manuelalferez",
+    },
+    {
+      alt: "Medium",
+      imageURL:
+        "https://ik.imagekit.io/manuelalferez/blog/social/medium_tqT52WAlI.svg",
+      url: "https://medium.com/me/stories/public",
+    },
+    {
+      alt: "LinkedIn",
+      imageURL:
+        "https://ik.imagekit.io/manuelalferez/blog/social/linkedin_1_EXSaznxE3s0.svg?updatedAt=1638464125147",
+      url: "https://www.linkedin.com/in/manuelalferez/",
+    },
+    {
+      alt: "Instagram",
+      imageURL:
+        "https://ik.imagekit.io/manuelalferez/blog/social/instagram_1_knxRwV2dp.svg",
+      url: "https://www.instagram.com/manuelalferez_",
+    },
+  ],
+  bgURL:
+    "https://ik.imagekit.io/manuelalferez/blog/bg/footer_p2tCzceOR.svg?updatedAt=1638454450091",
+  heartURL:
+    "https://ik.imagekit.io/manuelalferez/blog/social/love_fTeP6pmP_.svg",
+};
