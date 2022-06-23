f483a7c702cb88684ce337292f0bf2a60c07af82
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-06-23 17:14:34 +0200

    feat: small changes in the blog's design

--- components/Posts.js
+++ components/Posts.js
@@ -5,11 +5,11 @@ const Posts = ({ posts, path }) => {
   return (
     <div class="text-gray-600 body-font overflow-hidden">
       <div class="container px-5 py-12 mx-auto">
-        <div class="-my-8 divide-y-2 divide-gray-100">
+        <div class="-my-16 divide-y-2 divide-gray-100 md:w-3/6 xl:w-2/6 md:mx-auto">
           {posts.map((post) => {
             return (
-              <div class="py-8 md:flex md:flex-wrap ">
-                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+              <div class="py-6">
+                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col md:justify-center">
                   <span class="mt-1 text-gray-500 text-sm">{post.date}</span>
                 </div>
                 <div class="md:flex-grow">
@@ -21,8 +21,8 @@ const Posts = ({ posts, path }) => {
                       },
                     }}
                   >
-                    <a className="inline-flex items-center mt-4">
-                      <p class="text-xl font-medium text-gray-900 title-font mb-2">
+                    <a className="inline-flex items-center">
+                      <p class="text-lg font-medium text-gray-900 title-font mb-2 md:mt-2 lg:text-xl">
                         <RichText render={post.title} />
                       </p>
                     </a>
