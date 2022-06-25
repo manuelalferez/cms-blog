30b824ab150031340a0c76042e10bf8a64c48833
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-06-25 17:18:59 +0200

    design: simplified posts

--- components/Posts.js
+++ components/Posts.js
@@ -5,11 +5,11 @@ const Posts = ({ posts, path }) => {
   return (
     <div class="text-gray-600 body-font overflow-hidden">
       <div class="container px-5 py-12 mx-auto">
-        <div class="-my-16 divide-y-2 divide-gray-100 md:w-3/6 xl:w-2/6 md:mx-auto">
+        <div class="-my-16 md:w-3/6 xl:w-2/6 md:mx-auto">
           {posts.map((post) => {
             return (
-              <div class="py-6">
-                <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col md:justify-center">
+              <div class="py-4">
+                <div class="md:w-64 md:mb-0 mb-2 flex-shrink-0 flex flex-col md:justify-center">
                   <span class="mt-1 text-gray-500 text-sm">{post.date}</span>
                 </div>
                 <div class="md:flex-grow">
